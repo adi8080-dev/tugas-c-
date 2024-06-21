21b5064b067a6f71a1d4ea7c0674fe92d51f81f0
adi <pw880088@gmail.com>
2024-06-21 20:50:38 +0700

    tugas pertama

---
+++ contoh-utk-diperbaiki-2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+int i = 10;
+cout <<"nilai i="<<i<<endl;
+
+int j=77;
+cout <<"nilai j="<<j<<endl;
+
+return 0;
+
+
+
+}
---
+++ contoh-utk-diperbaiki.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+
+    int main()
+{
+
+
+        char c;
+        int i;
+        float x;
+
+        x=176.5;
+        i=x;
+        c=1;
+
+        cout <<"isi x="<<x<< endl;
+        cout <<"isi i="<<i<< endl;
+        cout <<"isi c="<<c<<endl;
+
+
+           return 0;
+
+
+}
